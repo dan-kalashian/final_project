0b44ea0d8040be0c7653e8e993bf3e05c75fd197
Dan Kalashian <dkalashi@terpmail.umd.edu>
2023-12-11 18:42:14 -0500

    Added comments and explainations

--- Final1_Program.py
+++ Final1_Program.py
@@ -1,27 +1,30 @@
-#Program
-#Develop a Word Scrapbook File using words from other files to make a story!
-
+#Program developes a Word Scrapbook File using words from other files to make a story!
+# Opens word collection one file, and writes that as 'WordScrapbook.txt' file. 
 with open("WordCollection1.txt", "r") as collection:
     part1=collection.read()
 with open("WordScrapbook.txt", "w") as Scrapbook:
     Scrapbook.write(part1)
 
+#Opens the second word collection, and appends that to add to 'WordScrapbook.txt' file. 
 with open("WordCollection2.txt", "r") as collection:
     part2=collection.read()
 with open("WordScrapbook.txt", "a") as Scrapbook:
     Scrapbook.write(part2)
 
+#Opens "WordScrapbook.txt" file and prints it
 with open ("WordScrapbook.txt", "r") as file:
     contents = file.read()
     print("Here is first scrapbook! " + contents)
 
 
 #Alternativeley, the scrapbook can lead to a story with a differnet ending using words from differnt files
+#Same as previous except for files being used
 with open("WordCollection1.txt", "r") as collection:
     part1=collection.read()
 with open("WordScrapbook.txt", "w") as Scrapbook:
     Scrapbook.write(part1)
 
+#Only difference is word collection 3 is not being read and appended
 with open("WordCollection3.txt", "r") as collection:
     part2=collection.read()
 with open("WordScrapbook.txt", "a") as Scrapbook:
@@ -33,36 +36,48 @@ with open ("WordScrapbook.txt", "r") as file:
 
 
 #Here, the user can choose how to end the story
+#User inputs which file they want to use to complete the story 
 end = input("Choose which file to use to end the story: (WordCollection2.txt or WordCollection3.txt): ")
+
+#same for the first part
 with open("WordCollection1.txt", "r") as collection:
     part1=collection.read()
 with open("WordScrapbook.txt", "w") as Scrapbook:
     Scrapbook.write(part1)
 
+#for the second part of story: program reads from user inputted file name
+#while loop
 a=0
 while a==0:
-        try:
+        try: # tried to read file with name user inputted, if succesful reads it
             with open(end, "r") as collection:
                 part2=collection.read()
             break
-        except FileNotFoundError:
+        except FileNotFoundError: # if error user will get to try again by selecting one or the other files. They can also quit
             print("file not found. Please type 'WordCollection2.txt' or 'WordCollection3.txt' or 'quit': ")
             end = input()
-            if end== "quit":
+            if end== "quit": # if user inputted one of the files correctly, program will have read it. If they quit, the story continues as such.
                 part2= "then went home and cries because you quit"
                 a=1
+
+#appends to add either words from user selected file or the "quit" endind
 with open("WordScrapbook.txt", "a") as Scrapbook:
     Scrapbook.write(part2)
 
+#opens WordScrapbook.txt to show user their story
 with open ("WordScrapbook.txt", "r") as file:
     contents = file.read()
     print("Here is your scrapbook! " + contents)
 
-#Also here is the gramatic structure of the words explained inside tuples. 
+#Grammar of the sentence is explained
+#tuple of words
 words = ("the", "man", "went", "to", "the", "store")
+#tuple assigned variables
 (article, subject, verb, preposition, preposition_article, object_of_the_preposition) = ("the", "man", "went", "to", "the", "store")
 print("I want to find out what the verb of this sentence is, so I will use the tuple!")
+#using tuple assigned definition of variable verb
 print("The verb is: " + verb)
+#can be done for any of the variables assigned with the words tuple
 print("Now I want to find out what the subject is")
 print("The subject is: "+ subject)
 
@@ -71,21 +86,27 @@ print("The subject is: "+ subject)
 #find the first section, and get just the text.
 import requests
 from bs4 import BeautifulSoup 
+#chosen website to webscrape with first paragraph explaining what grocery stores are
 URL = "https://dbpedia.org/page/Grocery_store"
+#bs4 and requests to scrape wesite and get it as Python objects?
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, 'html5lib') 
 print("After a trip to the store, I now love grocery stores. Here is some information about grocery stores online!")
+#Find the first instance of section, which is the first sectionin the website, which is the paragraph about grocery stores I wish to scrape
 grocery=soup.find("section")
 print(grocery.get_text())
-#print("Here is your scrapbook! " + contents + "Then the man thought of the quote: " + new)
 
-#API English class?
+#Using API to get English classes at UMD
 print("still interested in learning about grammar and writing? Take a look at the UMD Enlgish courses below!")
 import json
 import requests
 import re
+#obtain API data and get it as python object
 umd_request = requests.get("https://api.umd.io/v1/courses?dept_id=ENGL")
 umd_data=json.loads(umd_request.content)
+# for the courses in the API, if the dept id is ENGL, then print the Course ID and the name
+#this step became redundant as it was easiest for me to only obtain ENGL courses from the API given the API's contraints. 
+#However, this code still shows data extraction and what should be done if the API included all courses, so can be left. 
 for course in umd_data:
     course_id = course["course_id"]
     dept_id = course["dept_id"]
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 This is the README for my Final Project #1
-This program combines words from various txt files to create a "Scrapbook" that tells a small story. 
+
 The Final1_Program.py combines words from various txt files to create a "Scrapbook" that tells a small story. 
-Program allows user to input how they want the story to end. They do this by choosing which file they want to use to complete their story. 
+Program then allows user to input how they want the story to end. They do this by choosing which file they want to use to complete their story. 
 From there, grammatical elements of certain parts of the sentence are explained. 
 After that, the story continues to include the charachter looking up what grocery stores are online then sharing his findings
-After that, for users still interested in grammar, Enlgish classes available at UMD are reported
+After that, for users still interested in grammar, Enlgish classes available at UMD are shown
 
+Other files in this repo include the txt files that are used to make the work scrapbook. 
+Thank you for reading, and I hope you enjoy :)
